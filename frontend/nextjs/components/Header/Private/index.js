2ebebd91be272460ebff504caf3d782a04106f71
cris <cruztiempo@hotmail.com>
2020-04-20 12:53:44 -0400

    context conditional render

---
+++ frontend/nextjs/components/Header/Private/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+import { Layout } from 'antd';
+const { Header } = Layout;
+
+export default () => {
+    return (
+        <Header style={{ background: '#fff', padding: 0 }} />
+    )
+}
